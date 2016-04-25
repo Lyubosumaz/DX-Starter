91895c76d02ee544adf5e255807c685a5396ae29
xavortm <xavortm@gmail.com>
2016-04-25 20:23:11 +0300

    Move from grunt to gulp. Sass compile time to ~70ms from ~2000ms

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+	notify = require('gulp-notify');
+
+gulp.task('sass', function(){
+	return gulp.src('assets/sass/**/*.scss')
+	    .pipe(sass())
+    	.pipe(gulp.dest('assets/css/'));
+});
+
+gulp.task('watch', ['sass'], function() {
+	gulp.watch('assets/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
