c284e9a18d24185c63697fd7fc8b1b033e74f24e
xavortm <xavortm@gmail.com>
2016-06-13 13:54:18 +0300

    Add live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-	notify = require('gulp-notify');
+	notify = require('gulp-notify'),
+	livereload = require('gulp-livereload');
 
 gulp.task('sass', function(){
 	return gulp.src('assets/sass/**/*.scss')
 	    .pipe(sass())
 		.pipe(autoprefixer())
     	.pipe(gulp.dest('assets/css/'))
-		.pipe(notify("Sass compiled."));
+    	.pipe(livereload({ start: true }))
+		.pipe(notify({
+			onLast: true,
+			title: "Sass compiled successfully.",	
+			message: "Use livereload chrome extension"
+		}));
 });
 
 gulp.task('watch', ['sass'], function() {
+	livereload.listen();
 	gulp.watch('assets/sass/**/*.scss', ['sass']);
 });
 
