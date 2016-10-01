5c4ce51256edb9f5dea8e6c3448408a65f312029
xavortm <xavortm@gmail.com>
2016-10-01 11:22:21 +0300

    Begin working with prebuilds and snippets

--- header.php
+++ header.php
@@ -27,32 +27,34 @@
 	<header id="masthead" class="site-header">
 		<div class="row">
 			<div class="columns small-12">
-				<div class="site-branding">
-					<?php
-					if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-					endif;
+				<div class="site-header-inner">
+					<div class="site-branding">
+						<?php
+						if ( is_front_page() && is_home() ) : ?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php else : ?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php
+						endif;
 
-					$description = get_bloginfo( 'description', 'display' );
-					if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-					<?php
-					endif; ?>
-				</div><!-- .site-branding -->
+						$description = get_bloginfo( 'description', 'display' );
+						if ( $description || is_customize_preview() ) : ?>
+							<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+						<?php
+						endif; ?>
+					</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-						<i class="fa fa-reorder"></i>
-					</button>
-					<div class="menu-primary-container">
-						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-					</div><!-- .menu-primary-container -->
-				</nav><!-- #site-navigation -->
-			</div>
-		</div>
+					<nav id="site-navigation" class="main-navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+							<i class="fa fa-reorder"></i>
+						</button>
+						<div class="menu-primary-container">
+							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+						</div><!-- .menu-primary-container -->
+					</nav><!-- #site-navigation -->
+				</div><!-- .site-header-inner -->
+			</div><!-- /columns -->
+		</div><!-- /row -->
 	</header><!-- #site-header -->
 
 	<div id="content" class="site-content">
