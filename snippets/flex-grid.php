aeb7870f7735fac6709ae1ce3597b1f945d8a8f9
xavortm <xavortm@gmail.com>
2016-11-19 12:11:05 +0200

    Add more sections for the starter theme

--- snippets/flex-grid.php
+++ snippets/flex-grid.php
@@ -1,31 +1,41 @@
 <?php
 /**
  * A basic flex grid element type.
- * 
+ *
+ * @since  DX Starter v1.2.0
  * @uses  assets/sass/prebuilds/_flex-grid.scss
  */
 ?>
 
-<?php // Element only; Wrap it in a section for best use. ?>
-<div class="flex-grid-wrapper">
+<?php
+/**
+ * Element only; Wrap it in a section for best use.
+ * modifiers:
+ * 		.separators - adds padding and lines between the boxes
+ */
+?>
+<div class="flex-grid-wrapper separators">
 
 	<?php // uses .col-4 .col-3 .col-2. Has responsive styles ?>
 	<div class="flex-grid col-3">
 		<div class="grid-item">
 			<div class="your-box-element-here">
-				<span>Hello World</span>
-			</div><!-- .your-box-class-here -->
-		</div><!-- .grid-item -->
-		
-		<div class="grid-item">
-			<div class="your-box-element-here">
-				<span>Hello World</span>
+				<h3><i class="fa fa-star"></i> Example grid element heading</h3>
+				<p>Even worse than being over-busy is being distracted by things with little or no value — like mindlessly surfing the web when your child wants to play.</p>
 			</div><!-- .your-box-class-here -->
 		</div><!-- .grid-item -->
 
 		<div class="grid-item">
 			<div class="your-box-element-here">
-				<span>Hello World</span>
+				<h3><i class="fa fa-star"></i> Example grid element heading</h3>
+				<p>Even worse than being over-busy is being distracted by things with little or no value — like mindlessly surfing the web when your child wants to play.</p>
+			</div><!-- .your-box-class-here -->
+		</div><!-- .grid-item -->
+
+		<div class="grid-item">
+			<div class="your-box-element-here">
+				<h3><i class="fa fa-star"></i> Example grid element heading</h3>
+				<p>Even worse than being over-busy is being distracted by things with little or no value — like mindlessly surfing the web when your child wants to play.</p>
 			</div><!-- .your-box-class-here -->
 		</div><!-- .grid-item -->
 	</div><!-- .flex-grid -->
---
+++ templates/starter-allsections.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template Name: >> All sections <<
+ *
+ * Use this for reference when developing your themes. There
+ * are comments to help you out setitng up these sections. Feel free
+ * to use get_template_part or copy-paste the markup for more freedom.
+ *
+ * @since  v1.0.0
+ * @package DevriX_Starter
+ */
+
+get_header(); ?>
+
+<!-- This is a large background image with text overlaying it. -->
+<?php get_template_part( 'snippets/section', 'hero' ); ?>
+
+<div class="section-fullwidth inverted">
+	<div class="row">
+		<div class="columns">
+			<?php get_template_part( 'snippets/section', 'heading' ); ?>
+		</div><!-- .columns -->
+	</div><!-- .row -->
+	<div class="row">
+		<div class="columns">
+			<?php get_template_part( 'snippets/flex', 'grid' ); ?>
+		</div><!-- .columns -->
+	</div><!-- .row -->
+</div><!-- .section-fullwidth -->
+
+<!-- Left and right aligned image with text next to them -->
+<section class="section-fullwidth">
+	<div class="row">
+		<div class="column">
+			<?php get_template_part( 'snippets/section', 'heading' ); ?>
+			<?php get_template_part( 'snippets/section', 'halfsplit' ); ?>
+		</div><!-- .column -->
+	</div><!-- .row -->
+</section><!-- .section-fullwidth -->
+
+<div class="section-fullwidth border-top border-bottom">
+	<div class="row">
+		<div class="columns">
+			<?php get_template_part( 'snippets/section', 'heading' ); ?>
+		</div><!-- .columns -->
+	</div><!-- .row -->
+	<div class="row">
+		<div class="columns">
+			<?php get_template_part( 'snippets/flex', 'grid' ); ?>
+		</div><!-- .columns -->
+	</div><!-- .row -->
+</div><!-- .section-fullwidth -->
+
+<?php get_footer(); ?>
