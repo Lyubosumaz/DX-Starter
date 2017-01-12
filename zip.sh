680e40a2e189e77938cc2be5222c1d02aa45f8e7
xavortm <xavortm@gmail.com>
2017-01-12 13:35:31 +0200

    Exclude the bash script

--- zip.sh
+++ zip.sh
@@ -4,7 +4,7 @@ zip_name=$1
 
 if [[ -n "$zip_name" ]]; then
     echo "Zipping your theme without node_modules ..."
-    zip -r "$zip_name" ./ -x *.git* ./node_modules/**\* ./.sass-cache/**\* ./assets/sass/**\* ./README.md ./ziptheme.sh ./npm-debug.log ./tests ./changelog.txt ./gulpfile.js ./package.json "*.DS_Store" "*.editorconfig" "*.gitignore" "*.sass-cache/" "*.node_modules/" "tests/*" "assets/sass/"
+    zip -r "$zip_name" ./ -x *.git* ./node_modules/**\* ./.sass-cache/**\* ./assets/sass/**\* ./README.md ./ziptheme.sh ./npm-debug.log ./tests ./changelog.txt ./gulpfile.js ./package.json "*.DS_Store" "*.editorconfig" "*.gitignore" "*.sass-cache/" "*.node_modules/" "tests/*" "assets/sass/" "zip.sh"
 
     echo "Zip successfull."
 else
