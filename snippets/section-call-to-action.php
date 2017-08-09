dcc4f679c5a9b614360178e68fce1d7b2af46f16
Alex Dimitrov <xavortm@gmail.com>
2017-08-09 13:46:29 +0300

    Remove the .container class in call-to-action

--- snippets/section-call-to-action.php
+++ snippets/section-call-to-action.php
@@ -8,14 +8,12 @@
 ?>
 
 <section class="section-fullwidth inverted">
-	<div class="container">
-		<div class="row">
-			<div class="columns medium-9">
-				<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</h4>
-			</div>
-			<div class="columns medium-3 text-right">
-				<a href="#" class="button button-secondary">Learn more</a>
-			</div>
-		</div><!-- /end row -->
-	</div><!-- /end container -->
+	<div class="row">
+		<div class="columns medium-9">
+			<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</h4>
+		</div>
+		<div class="columns medium-3 text-right">
+			<a href="#" class="button button-secondary">Learn more</a>
+		</div>
+	</div><!-- /end row -->
 </section>
