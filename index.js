e573e152a9723ac36852f65e6f5d994d0eb449e4
Alex Dimitrov <xavortm@gmail.com>
2018-02-08 14:28:47 +0200

    Add some docs to the version script

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * Currently this script will generate a new version
+ * number for the theme's asset files. It writes in inc/theme-version.php
+ */
+
+// The two packages required to write in the theme-version.php file
 var fs = require('fs');
 var moment = require('moment');
 
@@ -9,21 +15,27 @@ fs.readFile(someFile, 'utf8', function (err,data) {
     return console.log(err);
   }
 
+  // The REGEX to find the assets version
+  let versionRegex = /define\( 'DX_ASSETS_VERSION', '*(.*?)' \);/g;
+  
   // Find the line where the version is at.
-  let re = /define\( 'DX_ASSETS_VERSION', '*(.*?)' \);/g;
-  let found = data.match(re);
+  let found = data.match(versionRegex);
 
-  // This will get and increment the version
+  // This will get and increment the version of the assets file.
   currentVersion = parseInt(found[0].substring(39, found[0].length - 4));
   currentVersion++;
 
+  // Create the date format. Of course you can change that to anything you like
+  // or even remove it so that you have only the version number and nothing else.
   let time = moment().format("YYYYMMDD");
 
-  let themeVersionNumber = time + '-' + currentVersion;
-  let themeVersionString = "define( 'DX_ASSETS_VERSION', '" + themeVersionNumber + "' );";
+  // Create the string that we will replace with the existing one.
+  let themeVersionString = "define( 'DX_ASSETS_VERSION', '" + time + '-' + currentVersion + "' );";
 
-  let result = data.replace(/define\( 'DX_ASSETS_VERSION', '*(.*?)' \);/g, themeVersionString);
+  // Once again find the string to replace and set the new version
+  let result = data.replace(versionRegex, themeVersionString);
 
+  // End it all by writing in the file (or throw error :) 
   fs.writeFile(someFile, result, 'utf8', function (err) {
      if (err) return console.log(err);
   });
