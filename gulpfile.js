10600c832908426ae7a32b9321046059fb614b1c
Alex Dimitrov <xavortm@gmail.com>
2018-02-21 17:13:22 +0200

    Improve scripts gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,59 @@
 'use strict';
 
-var 	gulp    	 = require('gulp'),
-		autoprefixer = require('gulp-autoprefixer'),
-		livereload   = require('gulp-livereload'),
-		sass         = require('gulp-sass'),
-		notify       = require('gulp-notify'),
-		uglify       = require('gulp-uglify'),
-		concat       = require('gulp-concat'),
-		cssmin       = require('gulp-cssmin'),
-		rename       = require('gulp-rename'),
-		imagemin     = require('gulp-imagemin'),
-		del          = require('del'),
-		moment       = require('moment'),
-		sassLint 	 = require('gulp-sass-lint');
+var gulp    	 = require('gulp'),
+	autoprefixer = require('gulp-autoprefixer'),
+	livereload   = require('gulp-livereload'),
+	sass         = require('gulp-sass'),
+	notify       = require('gulp-notify'),
+	uglify       = require('gulp-uglify'),
+	concat       = require('gulp-concat'),
+	cssmin       = require('gulp-cssmin'),
+	rename       = require('gulp-rename'),
+	imagemin     = require('gulp-imagemin'),
+	del          = require('del'),
+	moment       = require('moment'),
+	sassLint 	 = require('gulp-sass-lint');
 
 gulp.task('sass', function(){
 	return gulp.src('assets/sass/**/*.scss')
-		.pipe(sassLint({
-			files: {
-				ignore: [
-					'assets/sass/base/_normalize.scss',
-					'assets/sass/modularscale/**/*.scss',
-					'assets/sass/foundation/**/*.scss',
-					'assets/sass/font-awesome/**/*.scss'
-				]
-			}
-		}))
-		.pipe(sassLint.format())
-		.pipe(sassLint.failOnError())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer())
-		.pipe(gulp.dest('assets/css/'))
-		// .pipe(livereload({ start: true }))
-		.pipe(notify({
-			onLast: true,
-			title: "Sass compiled successfully.",
-			message: getFormatDate()
-		}));
-});
-
-gulp.task('scripts', function() {
-	return gulp.src('assets/scripts/scripts.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('./'))
-		.pipe(notify({ message: 'Scripts task complete <%= file.relative %>' }));
+	.pipe(sassLint({
+		files: {
+			ignore: [
+				'assets/sass/base/_normalize.scss',
+				'assets/sass/modularscale/**/*.scss',
+				'assets/sass/foundation/**/*.scss',
+				'assets/sass/font-awesome/**/*.scss'
+			]
+		}
+	}))
+	.pipe(sassLint.format())
+	.pipe(sassLint.failOnError())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(gulp.dest('assets/css/'))
+	// .pipe(livereload({ start: true }))
+	.pipe(notify({
+		onLast: true,
+		title: "Sass compiled successfully.",
+		message: getFormatDate()
+	}));
 });
 
 gulp.task('cssmin', function () {
 	gulp.src('assets/css/master.css')
-		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('assets/css'))
-		.pipe(notify({ message: 'Successfully minified master.min.css' }));
+	.pipe(cssmin())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('assets/css'))
+	.pipe(notify({ message: 'Successfully minified master.min.css' }));
 });
 
 // The files to be watched for minifying. If more dev js files are added this
 // will have to be updated.
-gulp.task('watch', ['sass', 'scripts'], function() {
+gulp.task('watch', ['sass'], function() {
 	livereload.listen();
 
 	gulp.watch('assets/sass/**/*.scss', ['sass']);
-	gulp.watch('assets/scripts/scripts.js', ['scripts', 'minifyScripts']);
+	gulp.watch('assets/scripts/scripts.js', ['minifyScripts']);
 	gulp.watch('assets/css/master.css', ['cssmin']);
 });
 
@@ -75,11 +68,11 @@ gulp.task('minifyScripts', function () {
 		'assets/scripts/inc/*.js',
 		'assets/scripts/scripts.js',
 	])
-		.pipe(concat('bundle.js'))
-		.pipe(gulp.dest('assets/scripts/'))
-		.pipe(rename('bundle.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('assets/scripts/'));
+	.pipe(concat('bundle.js'))
+	.pipe(gulp.dest('assets/scripts/'))
+	.pipe(rename('bundle.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('assets/scripts/'));
 });
 
 // This will take care of rights permission errors
@@ -114,6 +107,5 @@ gulp.task('default', ['sass', 'watch', 'minifyScripts', 'cssmin']);
 // Print the current date formatted. Used for the script compile notify messages.
 function getFormatDate() {
 	var currentTime = moment().format("LTS");
-
 	return currentTime;
 }
