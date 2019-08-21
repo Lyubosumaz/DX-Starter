dca75095c871a19049e8654b5ca18ff324fa1eb8
Martin Spatovaliyski <martinspatovaliyski@gmail.com>
2019-08-21 18:02:39 +0300

    Renames: Project renaming
    Make renaming a project more accessible by adjusting all files with a 
    single command and a ruleset

---
+++ initial-setup.js
@@ -0,0 +1,57 @@
+// The two packages required to write in the theme-version.php file
+var replace = require('replace');
+
+
+// Feel free to replace these with the name of the project that will be used. Keep it consistent within the name and follow the template
+
+// Lower case name. Example: projectname
+var lowercase = "projectname";
+
+// Lower case with a hyphen of the project. Example: project-name
+var lowercaseHyphen = "project-name";
+
+// Uppercase with a space between. Example: "Project Name"
+var uppercase = "Project Name";
+
+// Uppercase with an underscore. Example: "Project_Name"
+var uppercaseUnderscore = "Project_Name"
+
+
+// Replacements
+// TODO: Improve the excludes
+replace({
+    regex: "dxstarter",
+    replacement: lowercase,
+    paths: ['.'],
+    recursive: true,
+    silent: true,
+    exclude: 'initial-setup.js'
+});
+
+replace({
+    regex: "dx-starter",
+    replacement: lowercaseHyphen,
+    paths: ['./inc/.'],
+    recursive: true,
+    silent: true,
+    exclude: 'initial-setup.js'
+});
+
+replace({
+    regex: "DX Starter",
+    replacement: uppercase,
+    paths: ['./inc/.'],
+    recursive: true,
+    silent: true,
+    exclude: 'initial-setup.js'
+});
+
+replace({
+    regex: "DX_Starter",
+    replacement: uppercaseUnderscore,
+    paths: ['./inc/.'],
+    recursive: true,
+    silent: true,
+    exclude: 'initial-setup.js'
+});
+
