6beea2aedf360a70fb690ced382b3d97ac26308a
Lyuboslav <LyuboslavIlkov@gmail.com>
2020-02-08 21:46:59 +0200

    bugfix removing test icons

--- footer.php
+++ footer.php
@@ -11,18 +11,8 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 	<footer id="colophon" class="site-footer section-fullwidth">
-		<i class="fas fa-edit"></i>
-		<i class="fa fa-camera-retro fa-lg"></i>
-		<i class="fa fa-camera-retro fa-2x"></i>
-		<i class="fa fa-camera-retro fa-3x"></i>
-		<a class="btn-cta-freequote" href="#">Get a FREE Quote <i class="fa fa-arrow-right"></i></a>
-		<i class="fa fa-camera-retro fa-4x"></i>
-		<i class="fa fa-camera-retro fa-5x"></i>
-		<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
-		<span class="sr-only">Loading...</span>
-
 		<div class="row">
 			<div class="columns small-12">
 				<div class="site-info">
